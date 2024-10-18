82a2c3718bd459ac2d9131afc70d8270b7a64079
Devansh Indoriya <130271930+evansh28@users.noreply.github.com>
2024-10-18 12:14:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
       <div className="fixed top-0 -z-10 h-full w-full">
 
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#808_100%)]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#1AA_100%)]"></div>
       </div>
 
       <div className="container mx-auto px-8">
